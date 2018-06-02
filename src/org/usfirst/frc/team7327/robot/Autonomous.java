ef8b27ce6e2415779b348c2110c4ea6bce52b2b5
OxnardHighSchoolRobotics9 <OxnardHighSchoolRobotics@gmail.com>
2018-06-02 14:01:14 -0700

    REEEEEEEEEEEEEEEE
    begone normies, the meme king has arrived

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,64 +4,64 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
 	public static void Auto() {
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.TurnRight();
-		Sparky.TurnRight();
-		Sparky.TurnRight();
-		Sparky.TurnRight();
-		Sparky.TurnRight(); //Starboard
-		Sparky.MoveForward();
-		Sparky.TurnLeft(); //Stern
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.TurnRight(); 
-		Sparky.TurnRight(); //Inverse
-		Sparky.MoveForward();
-		Sparky.TurnLeft(); 
-		Sparky.TurnLeft();
-		Sparky.TurnLeft(); //Leftwards
-		Sparky.MoveForward();
-		Sparky.TurnLeft(); //Backwardo
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward(); //back to start
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.MoveForward();
-		Sparky.TurnRight();
-		Sparky.TurnRight(); //Forward
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.TurnRight();
+		Robot.TurnRight();
+		Robot.TurnRight();
+		Robot.TurnRight();
+		Robot.TurnRight(); //Starboard
+		Robot.MoveForward();
+		Robot.TurnLeft(); //Stern
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.TurnRight(); 
+		Robot.TurnRight(); //Inverse
+		Robot.MoveForward();
+		Robot.TurnLeft(); 
+		Robot.TurnLeft();
+		Robot.TurnLeft(); //Leftwards
+		Robot.MoveForward();
+		Robot.TurnLeft(); //Backwardo
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward(); //back to start
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.TurnRight();
+		Robot.TurnRight(); //Forward
 	}
 }
