96c7f545ec1d9a1d7c12feff221fcaf33ba0558b
OxnardHighSchoolRobotics9 <OxnardHighSchoolRobotics@gmail.com>
2018-06-02 13:46:16 -0700

    Scream
    Dream

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -5,7 +5,63 @@ import org.usfirst.frc.team7327.robot.Robot;
 public class Autonomous {
 	public static void Auto() {
 		Sparky.MoveForward();
-		Sparky.TurnRight();  
 		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.TurnRight();
+		Sparky.TurnRight();
+		Sparky.TurnRight();
+		Sparky.TurnRight();
+		Sparky.TurnRight(); //Starboard
+		Sparky.MoveForward();
+		Sparky.TurnLeft(); //Stern
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.TurnRight(); 
+		Sparky.TurnRight(); //Inverse
+		Sparky.MoveForward();
+		Sparky.TurnLeft(); 
+		Sparky.TurnLeft();
+		Sparky.TurnLeft(); //Leftwards
+		Sparky.MoveForward();
+		Sparky.TurnLeft(); //Backwardo
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward(); //back to start
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.TurnRight();
+		Sparky.TurnRight(); //Forward
 	}
 }
