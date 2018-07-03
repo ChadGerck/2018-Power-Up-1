a3104d83ac7c7cfda930077ae75b357b6984f32e
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-03 09:36:39 -0700

    a

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -6,9 +6,28 @@ public class Autonomous {
 	public static void Auto() {
 		Robot.MoveForward();
 		Robot.MoveForward();
-		Robot.TurnRight();
 		Robot.MoveForward();
+		Robot.MoveForward();
+	    Robot.MoveForward();
+	    Robot.MoveForward();
+	    Robot.MoveForward();   
+	    Robot.TurnRight();
+	    Robot.TurnRight();
+	    Robot.MoveForward();
+	    Robot.MoveForward();
+	    Robot.MoveForward();
+	    Robot.MoveForward();
+	    Robot.MoveForward();
+	    Robot.MoveForward();
+	    Robot.MoveForward();
+	
+	
+	
+	
 	}
+	
+	
+
 	
 }
 	    
