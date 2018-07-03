8d04d1d42d5af09dd66c2f1fef327d9f4c4b7c7b
OxnardHighSchoolRoboticsMAIN <OxnardHighSchoolRobotics@gmail.com>
2018-07-03 09:01:04 -0700

    Group3 Ready

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -6,62 +6,7 @@ public class Autonomous {
 	public static void Auto() {
 		Robot.MoveForward();
 		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
 		Robot.TurnRight();
-		Robot.TurnRight();
-		Robot.TurnRight();
-		Robot.TurnRight();
-		Robot.TurnRight(); //Starboard
-		Robot.MoveForward();
-		Robot.TurnLeft(); //Stern
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.TurnRight(); 
-		Robot.TurnRight(); //Inverse
-		Robot.MoveForward();
-		Robot.TurnLeft(); 
-		Robot.TurnLeft();
-		Robot.TurnLeft(); //Leftwards
-		Robot.MoveForward();
-		Robot.TurnLeft(); //Backwardo
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward(); //back to start
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.TurnRight();
-		Robot.TurnRight(); //Forward
+		Robot.MoveForward(); 
 	}
 }
