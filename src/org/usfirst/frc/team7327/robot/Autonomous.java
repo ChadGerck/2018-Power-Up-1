17451e03b73d4028258f53d51fcfae64a3db3395
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-03 09:46:51 -0700

    change in vars
    Change in vars.

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,14 +4,14 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
 	public static void Auto() {
-		for (int i = 1; i < 13; i++ ){
+		for (var i = 0; i < 13; i++ ){
 			Robot.MoveForward();
 		}
 		Robot.TurnLeft(); 
 		Robot.MoveForward();
 		Robot.MoveForward();
 		Robot.TurnLeft();
-		for (int i = 1; i < 13; i++ ){
+		for (var i = 0; i < 13; i++ ){
 			Robot.MoveForward();
 		}
 		Robot.TurnLeft();
