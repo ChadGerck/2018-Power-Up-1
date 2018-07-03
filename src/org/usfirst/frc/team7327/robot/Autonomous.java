93e738d52799ca8ecdd26d9f075255dedebe9806
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-03 10:18:25 -0700

    Robot to Sparky

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -1,23 +1,23 @@
 package org.usfirst.frc.team7327.robot;
 
-import org.usfirst.frc.team7327.robot.Robot;
+import org.usfirst.frc.team7327.robot.Sparky;
 
 public class Autonomous {
 	public static void Auto() {
 		int count = 0;
 		while(count < 13 ){
-			Robot.MoveForward();
+			Sparky.MoveForward();
 			count++;
 		}
-		Robot.TurnLeft(); 
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.TurnLeft();
+		Sparky.TurnLeft(); 
+		Sparky.MoveForward();
+		Sparky.MoveForward();
+		Sparky.TurnLeft();
 		while(count < 13) {
-			Robot.MoveForward();
+			Sparky.MoveForward();
 			count++;
 		}
-		Robot.TurnLeft();
-		Robot.TurnLeft();
+		Sparky.TurnLeft();
+		Sparky.TurnLeft();
 	}
 }
--- src/org/usfirst/frc/team7327/robot/Sparky.java
+++ src/org/usfirst/frc/team7327/robot/Sparky.java
@@ -21,3 +21,4 @@ public class Sparky {
 		if(!Offline) { Robot.TurnLeft(); }
 	}
 }
+
