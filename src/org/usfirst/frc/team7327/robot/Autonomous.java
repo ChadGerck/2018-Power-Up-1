f2569eb29ff20ae3a1a00b7446bff94d37a42e50
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-05 14:12:25 -0700

    Proyecto de Grupo 5

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,15 +4,51 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
 	public static void Auto() {
-		int i=0;
-		
-					while(i<52) {
-						Robot.MoveForward();
-						i++;
-						if (i==26|| i==52);
-							Robot.TurnRight();
-							Robot.TurnRight();
+		int station = 2;
+		int i = 0;
+		switch (station) {
+		case 1:
+			Robot.TurnLeft();
+			Robot.TurnLeft();
+			Robot.TurnLeft();
+			Robot.TurnLeft();
+			break;
+
+		case 2:
+			while (i < 2) {
+				Robot.TurnRight();
+				i++;
+			}
+			i = 0;
+			while (i < 4) {
+				Robot.MoveForward();
+				i++;
+			}
+
+			Robot.TurnRight();
+			Robot.TurnLeft();
+			Robot.TurnLeft();
+			Robot.TurnRight();
+			Robot.TurnRight();
+			Robot.TurnLeft();
+
+			break;
+
+		case 3:
+			Robot.TurnLeft();
+			Robot.TurnLeft();
+			Robot.MoveForward();
+			Robot.MoveForward();
+			Robot.MoveForward();
+			i = 0;
+			while (i < 4) {
+				Robot.TurnRight();
+				i++;
+			}
+
+			Robot.TurnLeft();
+			Robot.MoveForward();
+
 		}
 	}
 }
-	
