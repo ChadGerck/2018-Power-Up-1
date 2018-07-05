d89cb5dd38c4b63bdabc90d9358f6c65c29c1c5e
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-05 09:38:26 -0700

    Project 3
    Liam Bragg

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,8 +4,26 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
 	public static void Auto() {
-		Sparky.MoveForward();
-		Sparky.TurnRight();  
-		Sparky.MoveForward();
+		int i = 0;
+		while (i < 52) {
+			Robot.MoveForward();
+			i++;
+			if (i == 26) {
+				TurnAround();
+			}
+		}
+	}
+
+	public static void MoveForward(int distance) {
+		int i = 0;
+		while (i < distance) {
+			Robot.MoveForward();
+			i++;
+		}
+	}
+
+	public static void TurnAround() {
+		Robot.TurnRight();
+		Robot.TurnRight();
 	}
 }
