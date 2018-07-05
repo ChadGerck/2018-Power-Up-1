3b332aabac38a7a37735c59a67f762a014d5d6a5
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-05 09:31:43 -0700

    Project 2

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,62 +4,25 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
 	public static void Auto() {
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
+		int i = 0;
+		while(i<25) {
+			Robot.MoveForward();
+			i = i +1;
+		}
+
 		Robot.TurnLeft();
 		Robot.TurnLeft();
-		Robot.TurnLeft();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
+		int k = 0; 
+		while(k<25){
+			Robot.MoveForward();
+			k = k +1;
+		}
+	}
+	public static void MoveForward(int distance) {
+		int i = 0;
+		while(i<distance) {
+			Robot.MoveForward();
+			i = i +1;
+		}
 	}
 }
