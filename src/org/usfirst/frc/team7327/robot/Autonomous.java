7cfdd4ab457ab631feb6aeaf7d6eef11c7111e7c
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-05 09:00:33 -0700

    moveForward updated
    move forward 26 feet.

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -5,16 +5,21 @@ import org.usfirst.frc.team7327.robot.Sparky;
 
 public class Autonomous {
 	public static void Auto() {
+		
 		int count = 0;
-		while(count < 13 ){
+		
+		while(count < 26 ) {
 			Sparky.MoveForward();
 			count++;
 		}
+		
 		Sparky.TurnLeft(); 
 		Sparky.MoveForward();
 		Sparky.MoveForward();
 		Sparky.TurnLeft();
-		while(count < 13) {
+		
+		count = 0;
+		while(count < 26) {
 			Sparky.MoveForward();
 			count++;
 		}
