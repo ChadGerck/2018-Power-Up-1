3880e08d9d5f9b83ffd8cab49bf411a578d1d458
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-05 13:20:06 -0700

    project3

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,38 +4,45 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
 	public static void Auto() {
-		int i=0;
-		while(i<26) {
-			Robot.MoveForward();
-			i++;
+		int i = 0;
+	while (i <= 54) {
+		Robot.MoveForward();
+		
+		//sorry;
+		if (i == 27 || i == 54) {
+			Robot.TurnRight();
+			Robot.TurnRight();
 		}
-		Robot.TurnRight();
-		Robot.TurnRight();
-		
-		i=0;
-		while(i<26) {
-			Robot.MoveForward();
-			i++;
-		}
-		Robot.TurnRight();
-		Robot.TurnRight();
-		
-		
-		
-		
-		//moveForward(27);
-		//Robot.TurnRight();
-		//Robot.TurnRight();
-		//moveForward(27);
-		//Robot.TurnRight();
-		//Robot.TurnRight();
-		
+	i++;
+
 	}
-	public static void moveForward(int distance){
-		int i=0;
-				while (i<distance){
-					Robot.MoveForward();
-					i++;
-				}
+		
+		/* int i = 0;
+		while (i < 26) {
+			Robot.MoveForward();
+			i++;
+		}
+		Robot.TurnRight();
+		Robot.TurnRight();
+
+		i = 0;
+		while (i < 26) {
+			Robot.MoveForward();
+			i++;
+		}
+		Robot.TurnRight();
+		Robot.TurnRight();
+
+		// moveForward(27);
+		// Robot.TurnRight();
+		// Robot.TurnRight();
+		// moveForward(27);
+		// Robot.TurnRight();
+		 Robot.TurnRight();*/
+
+	}
+
+	public static void moveForward(int distance) {
+		
 	}
 }
