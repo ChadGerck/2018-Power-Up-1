2610a27b9ef73b4b069c4b51209d6ce23c0b1ada
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-05 14:11:24 -0700

    project 4

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -3,11 +3,76 @@ package org.usfirst.frc.team7327.robot;
 import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
+	
 	public static void Auto() {
-		int i = 0;
+	
+	int station = 3;
+			switch(station){
+			case 1 :
+				Robot.TurnRight();
+				Robot.TurnRight();
+				Robot.TurnRight();
+				Robot.TurnRight();
+				break;
+			case 2:
+				Robot.MoveForward();
+				Robot.MoveForward();
+				Robot.MoveForward();
+				Robot.MoveForward();	
+				Robot.TurnRight();
+				Robot.TurnRight();
+				Robot.MoveForward();
+				Robot.MoveForward();
+				Robot.MoveForward();
+				Robot.MoveForward();
+				Robot.TurnRight();
+				Robot.TurnRight();
+				Robot.MoveForward();
+				Robot.MoveForward();
+				Robot.MoveForward();
+				Robot.MoveForward();
+				break;
+			case 3:
+				Robot.TurnRight();
+				Robot.MoveForward();
+				Robot.MoveForward();
+				Robot.MoveForward();
+				Robot.TurnRight();	
+				Robot.MoveForward();
+				Robot.MoveForward();
+				Robot.MoveForward();
+				Robot.TurnRight();
+				Robot.MoveForward();
+				Robot.MoveForward();
+				Robot.MoveForward();
+				Robot.TurnRight();
+				Robot.MoveForward();
+				Robot.MoveForward();
+				Robot.MoveForward();
+				break;
+				
+			}
+	}
+	
+	// other challenge 
+	public static void MoveAngle(double angle) {
+		Robot.drivetrain.setRaw1(.25,  .25);
+		double gyroAngle = Robot.GyroAngle();
+		gyroAngle = Math.toRadians(gyroAngle);
+		
+		try{
+			Thread.sleep(20);
+		}
+		
+		catch(InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+		
+		
+		/*int i = 0;
 	while (i <= 54) {
 		Robot.MoveForward();
-		
 		//sorry;
 		if (i == 27 || i == 54) {
 			Robot.TurnRight();
@@ -17,7 +82,7 @@ public class Autonomous {
 
 	}
 		
-		/* int i = 0;
+		 int i = 0;
 		while (i < 26) {
 			Robot.MoveForward();
 			i++;
@@ -40,7 +105,7 @@ public class Autonomous {
 		// Robot.TurnRight();
 		 Robot.TurnRight();*/
 
-	}
+	
 
 	public static void moveForward(int distance) {
 		
