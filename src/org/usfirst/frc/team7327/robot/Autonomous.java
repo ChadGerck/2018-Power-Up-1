835630d6c3653d038a932432a25b6a2622eaa7ab
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-05 12:45:49 -0700

    Project 2

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -3,65 +3,29 @@ package org.usfirst.frc.team7327.robot;
 import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
-	public static void Auto() {
-		Robot.MoveForward(); 
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.TurnRight();
-		Robot.TurnRight();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.TurnRight();
-		Robot.TurnRight();
+	public static void Auto() {	//Robot.MoveForward(); 
+		//Robot.MoveForward();
+		//Robot.TurnRight();  
+		//Robot.MoveForward();
+		int i = 0;
+		while(i<26) { 	
+			Robot.MoveForward(); 
+			i = i +1;
+		}
 		
-		
+		//MoveForward(26);
+		while(i<2) { 
+			Robot.TurnRight(); 
+			i = i +1;
+		}
+		while(i<26) { 
+			Robot.MoveForward(); 
+			i = i +1;
+		}
+		while(i<2) { 
+			Robot.TurnRight(); 
+			i = i +1; 
+		}
 		
 	}
 }
