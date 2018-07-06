680af3bf4d57f66c90411bdb6dc049db896cf99b
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-06 09:35:02 -0700

    Project5

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,33 +4,21 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
 	public static void Auto() {
-		int station = 1; 
-		switch(station){
-		case 1:
-			Robot.MoveForward();
-			TurnAround();
-			Robot.MoveForward();
-			TurnAround();
-			break;
-		case 2:
-			Robot.MoveForward();
-			Robot.MoveForward();
-			Robot.MoveForward();
-			Robot.MoveForward();
-			Robot.MoveForward();
-			Robot.MoveForward();
-			Robot.MoveForward();
-			break;
-		case 3:
-			TurnAround();
-			TurnAround();
-			TurnAround();
-			TurnAround();
-			TurnAround();
-			TurnAround();
-			break;
+		double timer = Robot.myTimer.get();
+		if(timer < 5) {
+			Robot.drivetrain.setRaw1(.75, .375);
+		}
+		else if (timer < 10) {
+			Robot.drivetrain.setRaw1(-.25, .5); 
+		}
+		else if (timer < 15) {
+			Robot.drivetrain.setRaw1(.5, -.25);
+		}
+		else {
+			Robot.drivetrain.setRaw1(0, .0);
 		}
 	}
+	
 	public static void TurnAround() {
 		Robot.TurnRight();
 		Robot.TurnRight();
