e92b5fed7916dc83c896b4dd42648bb1a2841f29
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-06 14:35:20 -0700

    Project 6

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,77 +4,43 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
 	public static void Auto() {
-<<<<<<< HEAD
+
 	
 		while(true) {
 			double timer = Robot.myTimer.get();
 			//leftvaule and rightvalue from 0 - 1
-			Robot.drivetrain.setRaw1(0.25,  0.25);
+			//Robot.drivetrain.setRaw1(0.25,  0.25);
 			
-			if(timer < 10){
-				Robot.MoveForward();
+			
+			
+			if(timer < 2){
+				Robot.drivetrain.setRaw1(0.25, 0.25);
 			}
-			else if(timer<20); {
-				Robot.TurnRight();
+			else if(timer < 4) {
+				Robot.drivetrain.setRaw1(0.45,.45);
 			}
-		 if(timer<30) {
-				}
+			else if ( timer < 6) {
+				Robot.drivetrain.setRaw1(0.60, 0.60);
+			}	
+				else if(timer< 8) {
+				Robot.drivetrain.setRaw1(0.25, -0.25);
 			}
-		
-		}
-	}
-
-	    
-	    
-	
-	
-=======
-		int station = 2;
-		int i = 0;
-		switch (station) {
-		case 1:
-			Robot.TurnLeft();
-			Robot.TurnLeft();
-			Robot.TurnLeft();
-			Robot.TurnLeft();
-			break;
-
-		case 2:
-			while (i < 2) {
-				Robot.TurnRight();
-				i++;
+				else if (timer< 10) {
+				Robot.drivetrain.setRaw1(0.60, 0.60);
 			}
-			i = 0;
-			while (i < 4) {
-				Robot.MoveForward();
-				i++;
+				else if (timer< 12) {
+				Robot.drivetrain.setRaw1(0.45, 0.45);
+			}	
+				else if (timer< 12) {
+				Robot.drivetrain.setRaw1(0.25, 0.25);
 			}
-
-			Robot.TurnRight();
-			Robot.TurnLeft();
-			Robot.TurnLeft();
-			Robot.TurnRight();
-			Robot.TurnRight();
-			Robot.TurnLeft();
-
-			break;
-
-		case 3:
-			Robot.TurnLeft();
-			Robot.TurnLeft();
-			Robot.MoveForward();
-			Robot.MoveForward();
-			Robot.MoveForward();
-			i = 0;
-			while (i < 4) {
-				Robot.TurnRight();
-				i++;
-			}
-
-			Robot.TurnLeft();
-			Robot.MoveForward();
-
-		}
+	    }
 	}
 }
->>>>>>> f2569eb29ff20ae3a1a00b7446bff94d37a42e50
+		
+	
+
+	    
+	    
+	
+	
