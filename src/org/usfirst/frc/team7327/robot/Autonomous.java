89326f9d39cd033736762560455850102274f87b
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-06 13:23:50 -0700

    project 2 7/6/18

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -3,62 +3,28 @@ package org.usfirst.frc.team7327.robot;
 import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
+	
 	public static void Auto() {
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
+		int i = 0;
+		while(i < 26)
+		{
+			Robot.MoveForward();
+			i = i + 1;
+		}
+			
 		
 	    Robot.TurnRight();
 	    Robot.TurnRight();
 	    
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Robot.MoveForward();
+	    i = 0;
+		while(i < 26)
+		{
+			Robot.MoveForward();
+			i = i + 1;
+		}
+		
+		 Robot.TurnRight();
+		 Robot.TurnRight();
 	}
+	
 }
