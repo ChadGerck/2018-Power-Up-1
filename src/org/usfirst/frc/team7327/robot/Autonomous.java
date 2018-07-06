409a212d6035d8cd57a93901487684746bbb757a
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-06 14:40:14 -0700

    Project5

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -5,6 +5,27 @@ import org.usfirst.frc.team7327.robot.Robot;
 public class Autonomous {
 	
 	public static void Auto() {
+		
+		while(true) {
+			double timer = Robot.myTimer.get();
+			//leftvalue and rightvalue from 0 - 1
+			
+			if(timer<10) {
+				Robot.drivetrain.setRaw1(.25, .25);
+			}
+			else if ( timer < 20) {
+				Robot.drivetrain.setRaw1(.25, .50);
+			}
+			else if (timer<30) {
+				Robot.drivetrain.setRaw1(.50, .25);
+			}
+			else {
+				Robot.drivetrain.setRaw1(0, 0);
+			}
+		}
+		
+		
+		/*
 		int i = 0;
 		while(i < 52)
 		{
@@ -35,6 +56,7 @@ public class Autonomous {
 					
 				
 			}
+			*/
 		}
 			
 		
