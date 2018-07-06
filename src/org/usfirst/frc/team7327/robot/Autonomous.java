71b367cb9eafe41b9b7163e8beb688338f0866b2
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-06 13:35:55 -0700

    project 3 7/6/18

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -6,25 +6,20 @@ public class Autonomous {
 	
 	public static void Auto() {
 		int i = 0;
-		while(i < 26)
+		while(i < 52)
 		{
 			Robot.MoveForward();
 			i = i + 1;
+			
+			if(i == 25)
+			{
+				Robot.TurnRight();
+				Robot.TurnRight();
+			}
 		}
 			
 		
-	    Robot.TurnRight();
-	    Robot.TurnRight();
-	    
-	    i = 0;
-		while(i < 26)
-		{
-			Robot.MoveForward();
-			i = i + 1;
-		}
-		
-		 Robot.TurnRight();
-		 Robot.TurnRight();
+
 	}
 	
 }
