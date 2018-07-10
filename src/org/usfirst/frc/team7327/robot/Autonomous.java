787b07b547b8966ec9c5332f7b04d0042c3ee8a9
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-07-10 08:50:41 -0700

    Combined Code

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -3,68 +3,25 @@ package org.usfirst.frc.team7327.robot;
 import org.usfirst.frc.team7327.robot.Robot;
 
 
-	public class Autonomous {
-	public static void Auto() {
-	/*
-		int count = 0;
-		
-		while(count <= 52 ) {
+	public class Autonomous 
+	{
+		public static void Auto() 
+		{
+			//While Loops
+			Group0.WhileLoops();
 			
-			Sparky.MoveForward();
-			if (count == 26) {
-				Sparky.TurnLeft();
-				Sparky.TurnLeft();
-			}			
-			count++;
-		}		
-	}
-	public static void MoveForward(int distance){
-		int i = 0;
-		while (i < distance){
-			Sparky.MoveForward();
-			i = i + 1;
-			}
+			//1 while loop and if command
+			Group0.OneWhile();
+			
+			//switch
+			Group0.Switch(2);
+			
+			//Time if and if else
+			Group0.TimedIfElse();
 		}
-	*/
-	int station = 2;
-	switch(station){
-	case 1:
-		
-		Robot.MoveForward();
-		Robot.TurnRight();
-		Robot.MoveForward();
-		Robot.TurnLeft();
-		Shake();
-		
-		break;
-	case 2:
-		
-		Robot.MoveForward();
-		Robot.MoveForward();
-		Shake();
-		
-		break;
-	case 3:
-		
-		Robot.MoveForward();
-		Robot.TurnLeft();
-		Robot.MoveForward();
-		Robot.TurnRight();
-		Shake();
-		
-		break;
-	}
 	}
 	
-	public static void Shake(){
-		Robot.TurnRight();
-		Robot.TurnLeft();
-		Robot.TurnLeft();
-		Robot.TurnRight();
-		Robot.TurnRight();
-		Robot.TurnLeft();
-	}
+
 	
-	}
 
 
---
+++ src/org/usfirst/frc/team7327/robot/Group0.java
@@ -0,0 +1,157 @@
+package org.usfirst.frc.team7327.robot;
+
+public class Group0 
+{
+	public static void WhileLoops()
+	{
+		int counter = 0;
+		while (counter < 21)
+		{
+			Robot.MoveForward();
+			counter++;
+		}
+		Robot.TurnLeft();
+		Robot.TurnLeft();
+		counter = 0;
+		while (counter < 21)
+		{
+			Robot.MoveForward();
+			counter++;
+		}
+		
+	}
+	
+	public static void OneWhile()
+	{
+		int counter = 0;
+		while (counter < 42) 
+		{
+			Robot.MoveForward();
+			if (counter == 21)
+			{
+				Robot.TurnLeft();
+				Robot.TurnLeft();
+				counter++;
+			}
+			counter++;
+		}
+	}
+	
+	public static void Switch(int station)
+	{
+		switch(station){
+		case 1:
+			
+			Shake();
+			Robot.MoveForward();
+			Robot.TurnRight();
+			Robot.MoveForward();
+			Robot.TurnLeft();
+			Spin();
+			
+			break;
+		case 2:
+			
+			Robot.MoveForward();
+			Robot.MoveForward();
+			Shake();
+			Spin();
+			
+			break;
+		case 3:
+			
+			Shake();
+			Robot.MoveForward();
+			Robot.TurnLeft();
+			Robot.MoveForward();
+			Robot.TurnRight();
+			Spin();
+			
+			break;
+		}
+		}
+	
+	public static void TimedIfElse()
+	{
+		while(true) 
+		{
+			
+			double timer = Robot.myTimer.get();
+			
+			
+			if (timer < 5)
+			{
+				Robot.drivetrain.setRaw1(.25, .25);
+			} 
+			else if (timer < 10)
+			{
+				Robot.drivetrain.setRaw1(-.25, .25);
+			} 
+			else if (timer < 15)
+			{
+				Robot.drivetrain.setRaw1(.25,-.25);
+			}
+			else if (timer < 20)
+			{
+				Robot.drivetrain.setRaw1(.5,.5);
+			}
+			else if (timer < 25)
+			{
+				Robot.drivetrain.setRaw1(-.5,-.5);
+			}
+			else 
+			{
+				Robot.drivetrain.setRaw1(0, 0);
+			}
+		}
+	}
+	
+	public static void AccelerateDecelerate()
+	{
+		double leftSp = 0.01;
+		double rightSp = 0.01;
+		
+				while (leftSp <= 0.60 && rightSp <= 0.60)
+				{
+					leftSp = leftSp * 2;
+					rightSp = rightSp * 2;
+					Robot.drivetrain.setRaw1(leftSp, rightSp);
+					
+				}
+					
+				while (leftSp >= 0.00 && rightSp >= 0.00)
+				{
+					leftSp = leftSp / 2;
+					rightSp = rightSp / 2;
+					Robot.drivetrain.setRaw1(leftSp, rightSp);
+		
+				}
+		}
+	
+	public static void MoveForward(int distance)
+	{
+		int i = 0;
+		while (i > distance)
+		{
+			Robot.MoveForward();
+			i++;
+		}
+	}
+	
+	public static void Shake(){
+		Robot.TurnRight();
+		Robot.TurnLeft();
+		Robot.TurnLeft();
+		Robot.TurnRight();
+		Robot.TurnRight();
+		Robot.TurnLeft();
+	}
+	
+	public static void Spin()
+	{
+		Robot.TurnRight();
+		Robot.TurnRight();
+		Robot.TurnRight();
+		Robot.TurnRight();
+	}
+}
