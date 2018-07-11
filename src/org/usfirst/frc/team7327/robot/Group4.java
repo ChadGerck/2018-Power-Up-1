6d10cdbc7b0ca8750c42ed1efc80516236cc66f6
AngelMora <175124@oxnardunion.org>
2018-07-11 13:25:54 -0700

    Revert "Horacio"
    This reverts commit 226ebe6229d440442d8190fa43819a7c00bbc20c.

--- src/org/usfirst/frc/team7327/robot/Group4.java
+++ src/org/usfirst/frc/team7327/robot/Group4.java
@@ -1,30 +1,24 @@
-import org.usfirst.frc.team7327.robot.Robot;
+package org.usfirst.frc.team7327.robot;
 
 public class Group4 {
 	public static void Project2() {
-		// while loops
-
+		// While loops
 		int i = 0;
-		while (i < 26) {
-
+		while (i < 25) {
 			Robot.MoveForward();
 			i = i + 1;
 		}
 
-		 {
-
+		while (i < 2) {
 			Robot.TurnRight();
-			Robot.TurnRight();
-			
+			i = i + 1;
 
 		}
-
 	}
 
 	public static void Project3() {
 
 		// 1 while loop and if command
-
 		int i = 0;
 		while (i < 52) {
 			Robot.MoveForward();
@@ -34,50 +28,53 @@ public class Group4 {
 				Robot.TurnRight();
 				Robot.TurnRight();
 			}
-
 		}
-
 		Robot.TurnRight();
 		Robot.TurnRight();
-
 	}
 
 	public static void Project4() {
 
-		// switch
-		int station = 2;
-		switch(station){
-		case 1: 
-			while (i < 25){ 
-					Robot.MoveForward();
-					i = i +1; 
-			break; }
+		// Switch
+		int i = 1;
+		switch (i) {
+
+		case 1:
+			while (i < 10) {
+				Robot.MoveForward();
+				i = i + 1;
+				break;
+			}
+
 		case 2:
-			Robot.TurnLeft();
-			Robot.TurnLeft();
-			Robot.TurnLeft();
-			
+			while (i < 15) {
+				Robot.MoveForward();
+
+				while (i < 2)
+					Robot.TurnRight();
+				break;
+
+			}
+
 		}
-		
-		}
-		
 
 	}
-}
 
 	public static void Project5() {
+
+		// timed if, else if, else
+		{
+			if(10); {
+				Robot.MoveForward();
+				
+			else if(2) {
+				Robot.TurnLeft();
+				{else {
+					Robot.MoveForward();
+					
+				}
+			}
 		
-		//timed if, else if, else 
-		if (timer < 15);
-		Robot.MoveForward();
-		
-		
+		}
 	}
 }
-
-
-
-
-
-
-
