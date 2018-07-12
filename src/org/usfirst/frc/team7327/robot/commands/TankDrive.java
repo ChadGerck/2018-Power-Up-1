c868132721b7e5858b204c9b37d74cf09963cf80
Kc <kaitorito75@gmail.com>
2018-07-12 09:49:51 -0700

    Revert "Tank Drive Test"
    This reverts commit f67a2ba7f78b1923b835696b4346499d97a55f24.

--- src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
@@ -8,16 +8,12 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Servo;
 public class TankDrive extends Command {
-	XboxController Player1 = Robot.oi.Controller0;
-	double throttleL = 0.35;
-	double throttleX = 0.55;
 	public TankDrive() {
 		requires(Robot.drivetrain); 
 	}
 	protected void initialize() {
 	}
 	protected void execute(){
-		Robot.drivetrain.setRaw(Robot.oi.getLeftStickY(Player1)*throttleL,Robot.oi.getRightStickX(Player1)*throttleL,(Robot.oi.getRightTrigger(Player1)-Robot.oi.getLeftTrigger(Player1))*throttleX);
 	}
 	protected boolean isFinished() {
 		return false;
