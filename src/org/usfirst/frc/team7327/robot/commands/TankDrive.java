ce96dcca86a0e6676dc1349bb31c8484884b49d2
Fernando <Fernandv4@gmail.com>
2018-07-12 13:07:53 -0700

    new

--- src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
@@ -20,27 +20,29 @@ public class TankDrive extends Command {
 	}
 	XboxController Player1 = Robot.oi.Controller0; 
 	double throttleL = .25; 
+	double speed = .25; 
 	
 	protected void initialize() {
 	}
 	
 	protected void execute(){
 		if (Robot.oi.getRightTrigger(Player1) == 1){
-			throttleL = Math.abs(throttleL);
-			Robot.drivetrain.setRaw1(Robot.oi.getRightTrigger(Player1)*throttleL + Robot.oi.getLeftStickX(Player1)*throttleL, Robot.oi.getRightTrigger(Player1)*throttleL);
+			speed = Math.abs(speed);
+			//Robot.drivetrain.setRaw1(Robot.oi.getRightTrigger(Player1)*throttleL, Robot.oi.getRightTrigger(Player1)*throttleL);
 			
 			//sRobot.drivetrain.setRaw1(Robot.oi.getLeftStickX(Player1)*throttleL, 0);
 		}
 		
 		if (Robot.oi.getLeftTrigger(Player1) == 1){
-			throttleL = -Math.abs(throttleL);
-			Robot.drivetrain.setRaw1(Robot.oi.getLeftTrigger(Player1)*throttleL + Robot.oi.getLeftStickX(Player1)*throttleL, Robot.oi.getLeftTrigger(Player1)*throttleL);
+			speed = -Math.abs(speed);
+			//Robot.drivetrain.setRaw1(Robot.oi.getLeftTrigger(Player1)*throttleL, Robot.oi.getLeftTrigger(Player1)*throttleL);
 
 			
 			//throttleL = Math.abs(throttleL);			
 			//Robot.drivetrain.setRaw1(Robot.oi.getLeftStickX(Player1)*throttleL, 0);
 		}
-		
+			
+			Robot.drivetrain.setRaw1(speed + (Robot.oi.getLeftStickX(Player1)*throttleL), speed - (Robot.oi.getLeftStickX(Player1)*throttleL));
 
 			//Robot.drivetrain.setRaw1(Robot.oi.getLeftStickX(Player1)*throttleL, 0);
 			
