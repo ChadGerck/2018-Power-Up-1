7066309e7969693252b2fd43ac4a2b5aa24dd342
Michael David Pizano Jr <PizanoDMichael@gmail.com>
2018-07-12 10:23:12 -0700

    TankDrive

--- src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
@@ -9,15 +9,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Servo;
 public class TankDrive extends Command {
 	XboxController Player1 = Robot.oi.Controller0;
-	double throttleL = 0.35;
-	double throttleX = 0.55;
+	double throttleL = 0.45;
+	double throttleX = 0.75;
 	public TankDrive() {
 		requires(Robot.drivetrain); 
 	}
 	protected void initialize() {
 	}
 	protected void execute(){
-		Robot.drivetrain.setRaw(Robot.oi.getLeftStickY(Player1)*throttleL,Robot.oi.getRightStickX(Player1)*throttleL,(Robot.oi.getRightTrigger(Player1)-Robot.oi.getLeftTrigger(Player1))*throttleX);
+		Robot.drivetrain.setRaw(Robot.oi.getLeftStickY(Player1)*-throttleL,Robot.oi.getRightStickX(Player1)*-throttleL,(Robot.oi.getRightTrigger(Player1)-Robot.oi.getLeftTrigger(Player1))*throttleX);
 	}
 	protected boolean isFinished() {
 		return false;
