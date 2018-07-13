a75e662606db284347e8c96750896c706ee8e893
Kc <kaitorito75@gmail.com>
2018-07-13 09:24:35 -0700

    Kc's

--- src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
@@ -14,14 +14,51 @@ import edu.wpi.first.wpilibj.Servo;
 
 public class TankDrive extends Command {
     
+	XboxController Player1 = Robot.oi.Controller0;
+	double throttleL = 0.35;
+	double throttleK = 0.55;
+	DoubleSolenoid.Value Grabbers = DoubleSolenoid.Value.kOff;
+	DoubleSolenoid.Value Punchers = DoubleSolenoid.Value.kOff;
+	
 	public TankDrive() {
 		requires(Robot.drivetrain); 
 	}
+	
 	protected void initialize() {
 	}
 	
 	protected void execute(){
 		
+		
+	//	Robot.drivetrain.setRaw(Robot.oi.getLeftTrigger(Player1)*-throttleL, Robot.oi.getRightTrigger(Player1)*-throttleL, (Robot.oi.getLeftStickY(Player1))*throttleK);
+		
+		if(Robot.oi.getRightBumper(Player1)==true){
+			throttleL = Math.abs(throttleL)*-1;
+		}
+		if(Robot.oi.getRightBumper(Player1)==true){
+			throttleL = Math.abs(throttleL);
+		}
+		
+		Robot.drivetrain.setRaw(Robot.oi.getLeftTrigger(Player1)*throttleL, Robot.oi.getRightTrigger(Player1)*throttleL, (Robot.oi.getLeftStickY(Player1))*throttleK);
+		
+		
+		
+		if(Robot.oi.getYButton(Player1)) {
+			Grabbers = DoubleSolenoid.Value.kForward;
+			Robot.drivetrain.setRawGrabber(Grabbers);
+		if(Robot.oi.getAButton(Player1)) {
+			Grabbers = DoubleSolenoid.Value.kReverse;
+			Robot.drivetrain.setRawGrabber(Grabbers);
+		if(Robot.oi.getXButton(Player1)) {
+				Punchers = DoubleSolenoid.Value.kForward;
+				Robot.drivetrain.setPunchers(Punchers);
+		if(Robot.oi.getBButton(Player1)) { 
+				Punchers = DoubleSolenoid.Value.kReverse;
+				Robot.drivetrain.setPunchers(Punchers);
+					}
+				}
+			}
+		}
 
 	}
 
