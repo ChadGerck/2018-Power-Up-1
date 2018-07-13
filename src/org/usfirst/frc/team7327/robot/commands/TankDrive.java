6efa7b887c3b08ec6cba3073d89ebd4a62141634
Nancy <nancyambriz01@gmail.com>
2018-07-13 13:01:55 -0700

    TankDrive

--- src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
@@ -25,7 +25,11 @@ public class TankDrive extends Command {
 	}
 	protected void execute(){
 		
-		Robot.drivetrain.setRaw1(Robot.oi.getLeftStickY(Player1), Robot.oi.getRightStickY(Player1));
+		Robot.drivetrain.setRaw1(Robot.oi.getLeftStickY(Player1)+(Robot.oi.getLeftStickX(Player1))*throttleL, Robot.oi.getRightStickY(Player1)+Robot.oi.getRightStickX(Player1)*throttleL);
+		
+		
+		
+		
 		
 		
 		
