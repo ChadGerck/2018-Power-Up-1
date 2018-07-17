7e9614abab0528f6b4ec2ee6a324cb5d37603213
AngelMora <175124@oxnardunion.org>
2018-07-17 12:57:11 -0700

    Servos

--- src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
@@ -19,12 +19,25 @@ public class TankDrive extends Command {
 	XboxController Player1 = Robot.oi.Controller0;
 
 	double throttleL = .25;
+	double throttleA = .55;
+	
+	protected void initialize() {
+	}
+
+		Servo Servo1 = new Servo(1);
+		Servo Servo2 = new Servo(2);
+		
 
 	protected void execute() {
-		Robot.drivetrain.setRaw1(Robot.oi.getRightStickY(Player1)+ Robot.oi.getLeftStickX(Player1)* throttleL,
-				(-Robot.oi.getRightStickY(Player1)+ -Robot.oi.getLeftStickX(Player1)* throttleL));
-				
-			
+		
+		
+		if(Robot.oi.getBButton(Player1)) {
+			if(Servo1.get() == 0) {Servo1.setSpeed(1); }
+			else { Servo1.setSpeed(-1);;}
+		}
+		
+		Robot.drivetrain.setRaw1(-Robot.oi.getLeftTrigger(Player1), Robot.oi.getRightTrigger(Player1)* throttleL);
+	
 		Robot.drivetrain.setRawArm(Robot.oi.getLeftStickY(Player1)* throttleL);
 		
 		if(Robot.oi.Dpad(Player1) >= 0 && Robot.oi.Dpad(Player1) <= 45 || Robot.oi.Dpad(Player1) <= 360 && Robot.oi.Dpad(Player1) >= 315) {
@@ -40,6 +53,10 @@ public class TankDrive extends Command {
 			Robot.drivetrain.setRawSpinner(-0.3, -0.3);
 	
 		}
+	 {
+		Robot.drivetrain.setRawSpinner(0, 0);
+		
+	}
 	}
 
 	protected boolean isFinished() {
@@ -52,3 +69,11 @@ public class TankDrive extends Command {
 	}
 }
 // HELLLLOOOOOO!!!!!!
+
+
+
+
+	
+
+
+
