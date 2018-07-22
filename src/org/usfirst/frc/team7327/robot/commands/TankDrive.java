b7505e70f5aede292cf79cf92ef2cf1689fc6d2b
OxnardHighSchoolRoboticsMAIN <OxnardHighSchoolRobotics@gmail.com>
2018-07-21 17:10:22 -0700

    Starting Code

--- src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
@@ -19,30 +19,12 @@ public class TankDrive extends Command {
 	public static XboxController Player1 = Robot.oi.Controller0; 
 	protected void initialize() {
 	}
-	static double throttle = .65; 
-	static boolean fix = false; 
-
-	int wheel = -1; 
+	static double throttle = .25; 
 	protected void execute(){
 		
-		if(Robot.oi.getYButton(Player1)) { wheel = 0; }
-		if(Robot.oi.getBButton(Player1)) { wheel = 1; }
-		if(Robot.oi.getXButton(Player1)) { wheel = 2; }
-		if(Robot.oi.getAButton(Player1)) { wheel = 3; }
-		if(Robot.oi.getStartButton(Player1)) { wheel = 4; }
 		
-		switch(wheel) {
-		case -1: break; 
-		case 0: Robot.drivetrain.setRaw(Robot.oi.getLeftStickX(Player1), Robot.oi.getRightStickY(Player1), 0, 0, 0, 0, 0, 0); fix = false; break; 
-		case 1: Robot.drivetrain.setRaw(0, 0, Robot.oi.getLeftStickX(Player1), Robot.oi.getRightStickY(Player1), 0, 0, 0, 0); fix = false; break; 
-		case 2: Robot.drivetrain.setRaw(0, 0, 0, 0, Robot.oi.getLeftStickX(Player1), Robot.oi.getRightStickY(Player1), 0, 0); fix = false; break; 
-		case 3: Robot.drivetrain.setRaw(0, 0, 0, 0, 0, 0, Robot.oi.getLeftStickX(Player1), Robot.oi.getRightStickY(Player1)); fix = false; break; 
-		case 4: Robot.drivetrain.setRaw(Robot.oi.getLeftStickX(Player1)*throttle, (Robot.oi.getRightStickY(Player1)+Robot.oi.getRightTrigger(Player1))*throttle, Robot.oi.getLeftStickX(Player1)*throttle, (Robot.oi.getRightStickY(Player1)-Robot.oi.getRightTrigger(Player1))*throttle, Robot.oi.getLeftStickX(Player1)*throttle, (Robot.oi.getRightStickY(Player1)+Robot.oi.getRightTrigger(Player1))*throttle, Robot.oi.getLeftStickX(Player1)*throttle, (Robot.oi.getRightStickY(Player1)-Robot.oi.getRightTrigger(Player1))*throttle);
-			fix = true; 
-			break; 
-		}
 		
-		if(fix) {Robot.CorrectYourself(); }
+		Robot.CorrectYourself(); 
 		
 	}
 	
