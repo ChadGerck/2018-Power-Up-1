94952debae44d3e96d090576d701db027d6a4c36
Xecer21 <170740@oxnardunion.org>
2018-07-23 13:47:35 -0700

    new

--- src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
@@ -22,10 +22,10 @@ public class TankDrive extends Command {
 	static double throttle = .25; 
 	protected void execute(){
 		
-		Robot.drivetrain.setRaw(Robot.oi.getLeftStickX(Player1), Robot.oi.getRightStickY(Player1), 
-				Robot.oi.getLeftStickX(Player1), Robot.oi.getRightStickY(Player1), 
-				Robot.oi.getLeftStickX(Player1), Robot.oi.getRightStickY(Player1), 
-				Robot.oi.getLeftStickX(Player1), Robot.oi.getRightStickY(Player1));
+		Robot.drivetrain.setRaw(Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getRightStickY(Player1)*throttle, 
+				Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getRightStickY(Player1)*throttle, 
+				Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getRightStickY(Player1)*throttle, 
+				Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getRightStickY(Player1)*throttle);
 		
 		Robot.CorrectYourself(); 
 		
