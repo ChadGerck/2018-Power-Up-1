2e9ce2c95971f647f8938626d698f60702f275bd
liambragg69 <liambragg69@gmail.com>
2018-07-25 10:20:28 -0700

    I think that this is the final code

--- src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
@@ -1,17 +1,12 @@
 package org.usfirst.frc.team7327.robot.commands;
-
 import org.usfirst.frc.team7327.robot.Robot;
-
-
 //import com.ctre.phoenix.motorcontrol.ControlMode;
 //import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Servo;
-
 public class TankDrive extends Command {
 	XboxController Player1 = Robot.oi.Controller0;
 	XboxController Player2 = Robot.oi.Controller1;
@@ -20,7 +15,6 @@ public class TankDrive extends Command {
 	DoubleSolenoid.Value Grabbers = DoubleSolenoid.Value.kOff;
 	DoubleSolenoid.Value Punchers = DoubleSolenoid.Value.kOff;
 	boolean flag = true;
-	
 	public TankDrive() {
 		requires(Robot.drivetrain); 
 	}
