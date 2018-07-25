448b618f1b27f9e7aa856c5ecf82b7f674c6d7b0
Sara Centeno <sara.e.centeno18@gmail.com>
2018-07-25 13:31:26 -0700

    73s73r

--- src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team7327/robot/commands/TankDrive.java
@@ -22,10 +22,16 @@ public class TankDrive extends Command {
 	static double throttle = .25; 
 	protected void execute(){
 		
-		Robot.drivetrain.setRaw(Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getRightStickY(Player1)*throttle, 
-				Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getRightStickY(Player1)*throttle, 
-				Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getRightStickY(Player1)*throttle, 
-				Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getRightStickY(Player1)*throttle);
+		//Robot.drivetrain.setRaw(Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getRightStickY(Player1)*throttle, 
+				//Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getRightStickY(Player1)*throttle, 
+				//Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getRightStickY(Player1)*throttle, 
+				//Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getRightStickY(Player1)*throttle);
+		
+		Robot.drivetrain.setRaw(Robot.oi.getLeftStickY(Player1)*throttle, Robot.oi.getLeftStickX(Player1)*throttle,
+				Robot.oi.getLeftStickY(Player1)*throttle,  Robot.oi.getLeftStickX(Player1)*throttle,
+				Robot.oi.getLeftStickY(Player1)*throttle, Robot.oi.getLeftStickX(Player1)*throttle,
+				Robot.oi.getLeftStickY(Player1)*throttle,  Robot.oi.getLeftStickX(Player1)*throttle); 
+		
 		
 		Robot.CorrectYourself(); 
 		
@@ -42,3 +48,59 @@ public class TankDrive extends Command {
 		end();
 	}
 }
+
+/*
+ * package org.usfirst.frc.team7327.robot.commands;
+
+import org.usfirst.frc.team7327.robot.Robot;
+
+
+//import com.ctre.phoenix.motorcontrol.ControlMode;
+//import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Servo;
+
+public class TankDrive extends Command {
+	public TankDrive() {
+		requires(Robot.drivetrain); 
+	}
+	public static XboxController Player1 = Robot.oi.Controller0; 
+	
+	protected void initialize() {
+	} 
+	
+	static double throttle = .50; 
+	protected void execute(){ 
+		Robot.drivetrain.setRaw(Robot.oi.getLeftStickY(Player1)*throttle, Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getLeftStickY(Player1)*throttle,  Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getLeftStickY(Player1)*throttle, Robot.oi.getLeftStickX(Player1)*throttle, Robot.oi.getLeftStickY(Player1)*throttle,  Robot.oi.getLeftStickX(Player1)*throttle); 
+		
+		
+		
+		
+		Robot.CorrectYourself(); 
+		
+	}
+	
+	
+		
+		
+	
+
+	
+	protected boolean isFinished() {
+
+		return false;
+	}
+
+	protected void interrupted() {
+		end();
+	}
+}
+
+ */
+
+
+
