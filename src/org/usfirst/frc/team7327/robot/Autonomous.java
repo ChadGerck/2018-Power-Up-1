fd03c5a354077faf7f5564864344d1ab54c1c169
Xecer21 <170740@oxnardunion.org>
2018-07-27 13:53:14 -0700

    new

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -5,36 +5,16 @@ import org.usfirst.frc.team7327.robot.Robot;
 public class Autonomous {
 	public static void Auto() {
 
-	
-		while(true) {
-			double timer = Robot.myTimer.get();
-			//leftvaule and rightvalue from 0 - 1
-			//Robot.drivetrain.setRaw1(0.25,  0.25);
+	int i=0;
+	while( i < 16) {
+		Robot.MoveForward();
+		i++;
+		if(i == 8 || i == 16) {
+			Robot.TurnRight();
+			Robot.TurnRight();
 			
-			
-			
-			if(timer < 2){
-				Robot.drivetrain.setRaw1(0.25, 0.25);
 			}
-			else if(timer < 4) {
-				Robot.drivetrain.setRaw1(0.45,.45);
-			}
-			else if ( timer < 6) {
-				Robot.drivetrain.setRaw1(0.60, 0.60);
-			}	
-				else if(timer< 8) {
-				Robot.drivetrain.setRaw1(0.25, -0.25);
-			}
-				else if (timer< 10) {
-				Robot.drivetrain.setRaw1(0.60, 0.60);
-			}
-				else if (timer< 12) {
-				Robot.drivetrain.setRaw1(0.45, 0.45);
-			}	
-				else if (timer< 14) {
-				Robot.drivetrain.setRaw1(0.25, 0.25);
-			}
-	    }
+		}
 	}
 }
 		
