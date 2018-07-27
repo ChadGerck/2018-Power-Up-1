2624074244ab491a63c3c85511975a184debaaab
Ethan Ramirez <Ejrjmr@gmail.com>
2018-07-27 14:03:06 -0700

    Group 2

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -1,4 +1,3 @@
-//Hello I'm carlos
 package org.usfirst.frc.team7327.robot;
 
 import org.usfirst.frc.team7327.robot.Robot;
@@ -6,48 +5,14 @@ import org.usfirst.frc.team7327.robot.Robot;
 public class Autonomous {
 	public static void Auto() {
 		
+		MoveFoward(10);
+	}
 		
-		int station = 2;
-		switch(station){
-		case 1:
-			   Robot.TurnRight();
-		       Robot.TurnRight();
-		       Robot.TurnRight();
-		       Robot.TurnRight();
-		       
-		       
-		       break;
-		case 2:Robot.MoveForward();
-		       Robot.TurnRight();
-		       Robot.MoveForward();
-		       Robot.TurnRight();
-			
-			
-			   break;
-		case 3:Robot.TurnRight();
-		       Robot.TurnLeft();
-		       Robot.TurnRight();
-			   Robot.TurnLeft();
-			   
-			   
-			   break;
-
-		    } 
 		
-		int i = 0;
-		while(i<52){
-		    Robot.MoveForward();
-		    i++;
-			if(i == 25){
-			Robot.TurnRight();
-			Robot.TurnRight();
-			}
-		 }
 		
-	}  
 	public static void MoveFoward(int distance){
 		int i = 0;
-		while(i<26) {
+		while(i<distance) {
 		Robot.MoveForward();
 		i= i +1;
 		}
