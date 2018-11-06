3d0a0367cb24f91bd316850b751fa58b47e7999c
OxnardHighSchoolRoboticsMAIN <OxnardHighSchoolRobotics@gmail.com>
2018-11-06 07:48:00 -0800

    cleaning
    deleted TankDrive

--- src/org/usfirst/frc/team7327/robot/commands/SwerveDrive.java
+++ src/org/usfirst/frc/team7327/robot/commands/SwerveDrive.java
@@ -85,63 +85,25 @@ public class SwerveDrive extends Command {
 		if(A)  { setting = 0; Robot.drivetrain.turning.setOn(false); }
 		if(Lb) { setting = 0; Robot.drivetrain.turning.setOn(false); }
 		if(Rb) { setting = 1; Robot.drivetrain.turning.setOn(false); }
-		if(Y)  { setting = 3; Robot.drivetrain.turning.setOn(false); }
 		if(B)  { setting = 4; Robot.drivetrain.turning.setOn(false); }
 		
 		if(Robot.oi.Dpad(Player1) >= 0 ) { setting = 2; Robot.drivetrain.turning.setOn(true); }
-	
+		
+		
 		switch(setting) {
-		case 0: 
+		case 0: //Power Mode
 			Robot.drivetrain.setAllDegrees(setDegree+Robot.GyroAngle()+addDegree);
 			Robot.drivetrain.setAllSpeed(Ly-RT+LT);
 			break;
-		case 1:
+		case 1: //Turn Mode
 			Robot.drivetrain.setEachDegree(225, 315, 135, 45);
 			Robot.drivetrain.setAllSpeed(Lx+RT-LT);
 			break;
-		case 2: 
+		case 2: //D-Pad in Use
 			Robot.drivetrain.setEachDegree(225, 315, 135, 45);
 			Robot.drivetrain.turning.setYaw(Robot.oi.Dpad(Player1));
 			if(Robot.oi.Dpad(Player1) == -1) { setting = 0; Robot.drivetrain.turning.setOn(false); }
 			break; 
-		case 3: 
-			Robot.drivetrain.setAllDegrees(setDegree+Robot.GyroAngle()+addDegree);
-			//NW, NE, SW, SE
-			//sin(gyro), cos(gyro), -sin(gyro), -cos(gyro) 
-			
-			if(Robot.GyroAngle() >= 0 && Robot.GyroAngle() < 45 )
-			{
-				Robot.drivetrain.setEachSpeed((Ly-RT+LT+(Lx))*.5,(Ly-RT+LT-(Lx))*.5,(Ly-RT+LT+(Lx))*.5,(Ly-RT+LT-(Lx))*.5);
-			}
-			else if(Robot.GyroAngle() >= 45 && Robot.GyroAngle() < 90 )
-			{
-				Robot.drivetrain.setEachSpeed((Ly-RT+LT)*.5,(Ly-RT+LT-(Lx))*.5,(Ly-RT+LT+(Lx))*.5,(Ly-RT+LT)*.5);
-			}
-			else if(Robot.GyroAngle() >= 90 && Robot.GyroAngle() < 135 )
-			{
-				Robot.drivetrain.setEachSpeed((Ly-RT+LT-(Lx))*.5,(Ly-RT+LT-(Lx))*.5,(Ly-RT+LT+(Lx))*.5,(Ly-RT+LT+(Lx))*.5);
-			}
-			else if(Robot.GyroAngle() >= 135 && Robot.GyroAngle() < 180 )
-			{
-				Robot.drivetrain.setEachSpeed((Ly-RT+LT-(Lx))*.5,(Ly-RT+LT)*.5,(Ly-RT+LT)*.5,(Ly-RT+LT+(Lx))*.5);
-			}
-			else if(Robot.GyroAngle() >= 180 && Robot.GyroAngle() < 225 )
-			{
-				Robot.drivetrain.setEachSpeed((Ly-RT+LT-(Lx))*.5,(Ly-RT+LT+(Lx))*.5,(Ly-RT+LT-(Lx))*.5,(Ly-RT+LT+(Lx))*.5);
-			}
-			else if(Robot.GyroAngle() >= 225 && Robot.GyroAngle() < 270 )
-			{
-				Robot.drivetrain.setEachSpeed((Ly-RT+LT)*.5,(Ly-RT+LT+(Lx))*.5,(Ly-RT+LT-(Lx))*.5,(Ly-RT+LT)*.5);
-			}
-			else if(Robot.GyroAngle() >= 270 && Robot.GyroAngle() < 325 )
-			{
-				Robot.drivetrain.setEachSpeed((Ly-RT+LT+(Lx))*.5,(Ly-RT+LT+(Lx))*.5,(Ly-RT+LT-(Lx))*.5,(Ly-RT+LT-(Lx))*.5);
-			}
-			else if( Robot.GyroAngle() >= 325 && Robot.GyroAngle() <= 360 )
-			{
-				Robot.drivetrain.setEachSpeed((Ly-RT+LT+(Lx))*.5,(Ly-RT+LT)*.5,(Ly-RT+LT)*.5,(Ly-RT+LT-(Lx))*.5);
-			}
-			break;
 		case 4: //Precision Mode 
 			Robot.drivetrain.setAllDegrees(setDegree+Robot.GyroAngle()+addDegree);
 			Robot.drivetrain.setAllSpeed(-RT+LT);
@@ -151,6 +113,7 @@ public class SwerveDrive extends Command {
 			Robot.drivetrain.setEachDegree(225, 315, 135, 45);
 			Robot.drivetrain.turning.setYaw(degreesL);
 			if(magnitudeL <= .5) { setting = 4; Robot.drivetrain.turning.setOn(false); }
+			break; 
 		}
 		
 	}
