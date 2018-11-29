6375277c206311891697a0041ef331ec82b59a88
AngelMora <175124@oxnardunion.org>
2018-11-28 16:47:07 -0800

    cleaning auto code

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -5,16 +5,7 @@ import org.usfirst.frc.team7327.robot.Robot;
 public class Autonomous {
 	public static void Auto() {
 
-	int i=0;
-	while( i < 16) {
-		Robot.MoveForward();
-		i++;
-		if(i == 8 || i == 16) {
-			Robot.TurnRight();
-			Robot.TurnRight();
-			
-			}
-		}
+		
 	}
 }
 		
