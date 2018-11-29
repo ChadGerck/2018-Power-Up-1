3985cc755e9abc79e4ce463f8a2ca3d3b9e90fba
Certaingemstone <jade.chongsathapornpong@gmail.com>
2018-11-28 16:47:24 -0800

    Update Autonomous.java

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -5,17 +5,20 @@ import org.usfirst.frc.team7327.robot.Robot;
 public class Autonomous {
 	public static void Auto() {
 		
-		MoveFoward(10);
 	}
 		
 		
 		
-	public static void MoveFoward(int distance){
+	public static void MoveForward(int distance){
 		int i = 0;
 		while(i<distance) {
 		Robot.MoveForward();
 		i= i +1;
 		}
+	}
+	
+	public static void HelloChad() {
+		
 	}
 
 
