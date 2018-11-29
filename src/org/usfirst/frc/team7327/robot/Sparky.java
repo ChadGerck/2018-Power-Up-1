556a05e167fffdb3ac4b23d1680603b0c5ca391f
Ansel <anselayala13@gmail.com>
2018-11-28 16:17:58 -0800

    yeah

--- src/org/usfirst/frc/team7327/robot/Sparky.java
+++ src/org/usfirst/frc/team7327/robot/Sparky.java
@@ -3,61 +3,32 @@ package org.usfirst.frc.team7327.robot;
 public class Sparky {
 	public static boolean Offline = false; 
 	
-	public static void main(String[] args) {
-		//Offline = true; 
+	public static void main(String[] args) throws InterruptedException {
+		Offline = true; 
 		//Autonomous.Auto(); 
 		
-		int i = 1;
-		while (i>1){
-			
-		System.out.println(i);
-		i = (i) / 2;
-		}
-	}
 		
-		
-		
-		
-		
-		
-		
+		System.out.println("a");
+		System.out.println("b");
+		System.out.println("c");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
 		
 		
 	}
-	//public static void MoveForward() {
-		//System.out.println("Move robot forward 1 space");
-		//if(!Offline) { Robot.MoveDistance(Robot.GyroAngle(), .3, 1); }
-	
-	//public static void TurnRight() {
-		//System.out.println("Turn Right");
-		//if(!Offline) { Robot.TurnRight(); }
-	
-	//public static void TurnLeft() {
-		//System.out.println("Turn Left");
-		//if(!Offline) { Robot.TurnLeft(); }
 	/*
-
-package edu.wpi.first.wpilibj.templates;
-
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.SimpleRobot;
-import edu.wpi.first.wpilibj.Timer;
-
-public class RobotTemple extands SimpleRobot{
-
-	
-	
-	
-	
-
-
-	public void operationControl() {
-		
-		while (limitSwitch.get()) {
-			Timer.delay(10);
-			}
-			
-			
-		}
+	public static void MoveForward() {
+		System.out.println("Move robot forward 1 space");
+		if(!Offline) { Robot.MoveDistance(Robot.GyroAngle(), .3, 1); }
 	}
-*/
+	public static void TurnRight() {
+		System.out.println("Turn Right");
+		if(!Offline) { Robot.TurnRight(); }
+	}
+	public static void TurnLeft() {
+		System.out.println("Turn Left");
+		if(!Offline) { Robot.TurnLeft(); }
+	}
+	*/
+}
