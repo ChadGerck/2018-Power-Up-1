d7c4f302820c9fa3952a18a9056496dfc51096a9
AngelMora <175124@oxnardunion.org>
2018-11-28 16:46:21 -0800

    clean auto code

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,31 +4,6 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
 	public static void Auto() {
-		double timer = Robot.myTimer.get(); 
-		while (true) {
-			Robot.drivetrain.setRaw1(.1, .1);
-
-			 if ( timer < 10) {
-			Robot.drivetrain.setRaw1(.25, .25);}
-			 
-			 if (timer < 20) {
-			Robot.drivetrain.setRaw1(.50, .50);}
-			 
-			 if (timer < 30) {
-			Robot.drivetrain.setRaw1(0, 0); }
-			 
-			 if (timer < 40) {
-			Robot.drivetrain.setRaw1(-.50, -.50); }
-			 
-			 if (timer < 50) {
-			Robot.drivetrain.setRaw1(-.25, -.25); }
-			 
-			 if (timer < 60) {
-			Robot.drivetrain.setRaw1(-.1, -.1); }
-			 
-			 if (timer < 70) {
-			Robot.drivetrain.setRaw1(0, 0); }
-
-		}
+		
 	}
 }
