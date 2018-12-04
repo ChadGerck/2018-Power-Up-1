56749e57cec49c73d521df67ccf89ead770c1759
Ansel <anselayala13@gmail.com>
2018-12-03 16:35:05 -0800

    First Try
    ??

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,8 +4,35 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
 	public static void Auto() {
+	
+		int i = 0;
 		
+	while (i < 34) {
+		i = i + 1;
+		Robot.MoveForward();
+	}		
+	
+		Robot.TurnRight();
+	
+	while (i < 6) {
+		i = i + 1;
+		Robot.MoveForward();
+	}
+	
+		Robot.TurnRight();
+		Robot.TurnRight();
 		
+	while (i < 6) {
+		i = i + 1;
+		Robot.MoveForward();
+		}
 		
+		Robot.TurnLeft();
+		
+	while (i < 34) {
+		i = i + 1;
+		Robot.MoveForward();
+		}
+	
 	}
 }
