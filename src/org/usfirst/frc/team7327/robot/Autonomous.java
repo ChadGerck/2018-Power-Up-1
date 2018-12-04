e9122f42a34fd41fee5d90c8aee07bb99471998f
Ansel <anselayala13@gmail.com>
2018-12-03 16:44:51 -0800

    Changed Counter locations

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -8,30 +8,30 @@ public class Autonomous {
 		int i = 0;
 		
 	while (i < 34) {
-		i = i + 1;
 		Robot.MoveForward();
+		i = i + 1;
 	}		
 	
 		Robot.TurnRight();
 	
 	while (i < 6) {
-		i = i + 1;
 		Robot.MoveForward();
+		i = i + 1;
 	}
 	
 		Robot.TurnRight();
 		Robot.TurnRight();
 		
 	while (i < 6) {
-		i = i + 1;
 		Robot.MoveForward();
+		i = i + 1;
 		}
 		
 		Robot.TurnLeft();
 		
 	while (i < 34) {
-		i = i + 1;
 		Robot.MoveForward();
+		i = i + 1;
 		}
 	
 	}
