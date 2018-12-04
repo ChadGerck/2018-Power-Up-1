855c054da47db58a2c14592fc2370ff32cc173a2
Alex Cordova <alexxcordovaa@gmail.com>
2018-12-03 16:53:46 -0800

    Alex's Code

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -4,109 +4,35 @@ import org.usfirst.frc.team7327.robot.Robot;
 
 public class Autonomous {
 	public static void Auto() {
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
+     int a = 0;
+     while(a < 33) {
+    	 Robot.MoveForward();
+    	 a = a + 1;
+     }
+	 
 	 Robot.TurnLeft();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
+	 
+	 int b = 0;
+	 while(b < 15) {
+		 Robot.MoveForward();
+		 b = b + 1;
+	 }
+	 
 	 Robot.TurnLeft();
 	 Robot.TurnLeft();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
+	 
+	 int c = 0;
+	 while(c < 15) {
+		 Robot.MoveForward();
+		 c = c + 1;
+	 }
+	 
 	 Robot.TurnRight();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	 Robot.MoveForward();
-	}	 
+	 
+	 int d = 0;
+     while(d < 33) {
+    	 Robot.MoveForward();
+    	 d = d + 1;
+     }
+	 } 
 }
