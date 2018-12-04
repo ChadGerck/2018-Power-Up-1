61641d99f55b2caf0b1746e8ece1eaacd6a27ec0
OxnardHighSchoolRobotics <38021865+OxnardHighSchoolRobotics@users.noreply.github.com>
2018-12-03 16:10:14 -0800

    Me and Kenny are beast
    I

--- src/org/usfirst/frc/team7327/robot/Autonomous.java
+++ src/org/usfirst/frc/team7327/robot/Autonomous.java
@@ -9,8 +9,107 @@ public class Autonomous {
 	public static void auto() {
 			
 		Robot.MoveForward();
-		Robot.TurnRight();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		
+		Robot.TurnLeft();
+		
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		
+		Robot.TurnLeft();
+		Robot.TurnLeft();
+		
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
 	
+		Robot.TurnRight();
+		
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
+		Robot.MoveForward();
 		}
 	}
 			
